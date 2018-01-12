518e0f04557dc1b4fa83d9d577bfb4369600f4e1
carsonluuu <34802086+carsonluuu@users.noreply.github.com>
2018-01-11 17:11:26 -0800

    Add files via upload

---
+++ machine-learning-ex6/machine-learning-ex6/ex6/dataset3Params.m
@@ -0,0 +1,52 @@
+function [C, sigma] = dataset3Params(X, y, Xval, yval)
+%DATASET3PARAMS returns your choice of C and sigma for Part 3 of the exercise
+%where you select the optimal (C, sigma) learning parameters to use for SVM
+%with RBF kernel
+%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and 
+%   sigma. You should complete this function to return the optimal C and 
+%   sigma based on a cross-validation set.
+%
+
+% You need to return the following variables correctly.
+C = 1;
+sigma = 0.3;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Fill in this function to return the optimal C and sigma
+%               learning parameters found using the cross validation set.
+%               You can use svmPredict to predict the labels on the cross
+%               validation set. For example, 
+%                   predictions = svmPredict(model, Xval);
+%               will return the predictions on the cross validation set.
+%
+%  Note: You can compute the prediction error using 
+%        mean(double(predictions ~= yval))
+%
+C_array = [0.01 0.03 0.1 0.3 1 3 10 30];
+sigma_array = [0.01 0.03 0.1 0.3 1 3 10 30];
+minError = 1000;
+error = zeros(8,8);
+minC = 0;
+minSigma = 0;
+
+for i =1:8,
+    for j = 1:8,
+        model= svmTrain(X, y, C_array(i), @(x1, x2) gaussianKernel(x1, x2, sigma_array(j))); 
+        C = C_array(:,i);
+        sigma = sigma_array (:,j);
+        predictions = svmPredict(model, Xval);
+        error(i,j) = mean(double(predictions~= yval));
+        %fprintf('%f %f %f\n',C_array(i),sigma_array(j),error(i,j)); 
+        if(error(i,j) < minError)
+            minError = error(i,j);
+            minC = C_array(:,i);
+            minSigma = sigma_array (:,j);
+        end
+    end
+end
+C = minC;
+sigma = minSigma ;
+%fprintf('%f and %f perform best, the error is %f',C,sigma,minError);
+% =========================================================================
+
+end
